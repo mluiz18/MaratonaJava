e25fbfa4cfd3f9773b5f3a48fea8299b2f6a4f9d
mluiz18 <migueluiz18@gmail.com>
2025-01-25 17:35:36 -0300

    Arquivos iniciais de estudo

---
+++ src/Aula01_OlaDevDojo.java
@@ -0,0 +1,7 @@
+public class Aula01_OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Olá, DevDojo!");
+        System.out.println("Olá Pelé! King of football!");
+        System.out.println("Olá Mark! King of zap zap!");
+    }
+}
