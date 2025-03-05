08999dc1964644dd6457b2c316c162105d95b72f
mluiz18 <migueluiz18@gmail.com>
2025-03-05 16:08:30 -0300

    docs: Aulas sobre estruturas de repetição e exercícios

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06_EstruturasDeRepetição03.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06_EstruturasDeRepetição03 {
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000){
+                continue;
+            }
+            System.out.println("Parcela = " + parcela + ", Valor da parcela = R$" + valorParcela);
+        }
+    }
+}
