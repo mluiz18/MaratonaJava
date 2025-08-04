15515fec8da2d65ade9ab9b8112007e1180d1865
mluiz18 <migueluiz18@gmail.com>
2025-08-04 20:32:05 -0300

    docs: "Orientação a Objetos: Classes 02"

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTeste02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        Estudante estudante1 = new Estudante();
+
+        estudante.Pessoa = "Ricardo";
+        System.out.println(estudante.Pessoa);
+        System.out.println(estudante.Idade);
+        System.out.println(estudante.Sexo);
+        System.out.println("-------------------");
+        System.out.println(estudante1.Pessoa);
+        System.out.println(estudante1.Idade);
+        System.out.println(estudante1.Sexo);
+    }
+}
