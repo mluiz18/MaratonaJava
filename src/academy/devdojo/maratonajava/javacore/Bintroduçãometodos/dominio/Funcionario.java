7400f364b5f94b781afe045184ec5c0cb0eda5ab
mluiz18 <migueluiz18@gmail.com>
2025-08-13 14:31:04 -0300

    docs: "Orientação a Objetos: Modificadores de Acesso"

--- src/academy/devdojo/maratonajava/javacore/Bintroduçãometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãometodos/dominio/Funcionario.java
@@ -1,22 +1,53 @@
 package academy.devdojo.maratonajava.javacore.Bintroduçãometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double salario;
+    private String nome;
+    private int idade;
+    private double[] salario;
+    private double media = 0;
 
     public void imprimir() {
         System.out.println(this.nome);
         System.out.println(this.idade);
-        System.out.println(this.salario);
+        for (double sal : this.salario) {
+            System.out.println(sal);
+        }
+        mediaSalarios(this.salario);
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=");
     }
 
     public void mediaSalarios(double... sals) {
-        double soma = 0;
         for (double sal : sals) {
-            soma += sal;
+            this.media += sal;
         }
-        System.out.println("Média salários = " + (soma/3));
+        System.out.println("Média salários = " + (media/3));
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double[] salario) {
+        this.salario = salario;
+    }
+
+    public double getMedia() {
+        return media / 3;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroduçãometodos/test/FuncionarioTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroduçãometodos/test/FuncionarioTeste01.java
@@ -4,31 +4,12 @@ import academy.devdojo.maratonajava.javacore.Bintroduçãometodos.dominio.Funcio
 
 public class FuncionarioTeste01 {
     public static void main(String[] args) {
-        Funcionario funcionario01 = new Funcionario();
-        Funcionario funcionario02 = new Funcionario();
-        Funcionario funcionario03 = new Funcionario();
+        Funcionario funcionario = new Funcionario();
 
-        funcionario01.nome = "Miguel";
-        funcionario01.idade = 18;
-        funcionario01.salario = 5000F;
-
-        funcionario02.nome = "Luiza";
-        funcionario02.idade = 18;
-        funcionario02.salario = 5000F;
-
-        funcionario03.nome = "Ignácio";
-        funcionario03.idade = 32;
-        funcionario03.salario = 5000F;
-
-        double[] salarios = new double[4];
-        salarios[1] = funcionario01.salario;
-        salarios[2] = funcionario02.salario;
-        salarios[3] = funcionario03.salario;
-
-        funcionario01.imprimir();
-        funcionario02.imprimir();
-        funcionario03.imprimir();
-
-        funcionario01.mediaSalarios(salarios);
+        funcionario.setNome("Luiza");
+        funcionario.setIdade(18);
+        funcionario.setSalario(new double[]{1200,987.32,2000});
+        funcionario.imprimir();
+        System.out.println(funcionario.getMedia());
     }
 }
